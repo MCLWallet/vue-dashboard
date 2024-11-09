78b622b59834da40adf1052f04c9a9d86d258e4a
Marcell Lanczos <e1209638@student.tuwien.ac.at>
2024-11-09 15:35:25 +0100

    Adds Film type

---
+++ src/types/Film.ts
@@ -0,0 +1,19 @@
+export type Film = {
+  Position: string;
+  Const: string;
+  Created: string;
+  Modified: string;
+  Description: string;
+  Title: string;
+  URL: string;
+  TitleType: string;
+  IMDbRating: string;
+  RuntimeMins: string;
+  Year: string;
+  Genres: string;
+  NumVotes: string;
+  ReleaseDate: string;
+  Directors: string;
+  YourRating: string;
+  DateRated: string;
+};
