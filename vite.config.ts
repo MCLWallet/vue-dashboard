7e1662bd1f362aee27a433482a161545db9593e2
Marcell Lanczos <e1209638@student.tuwien.ac.at>
2024-11-09 15:35:15 +0100

    Imports bootstrap & sass + adds proxy for what-to-watch API

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://fixed-kindly-guardian.glitch.me/watchlist?genres=Crime",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+});
